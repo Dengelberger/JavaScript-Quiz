15dde07076b4914acde88dec8aa182008ea177e8
Donna Engelberger <dengelberger@gmail.com>
2020-09-08 16:12:37 -0400

    MOST THINGS FIXED. WORKING ON GAME OVER.

--- script.js
+++ script.js
@@ -56,7 +56,9 @@ var myJavaQuizQuestions = [
 var startEl = document.querySelector("#startBtn");    
 var welcomeEL = document.querySelector(".welcomePage");
 
-startEl.addEventListener("click", function() {
+startEl.addEventListener("click", function(event) {
+    event.preventDefault();
+    setTime();
     var questionBox = document.getElementById("questionBox");       
 
     // THIS PART IS TO HIDE THE WELCOME ON THE BEGINNING ONCE THE START BUTTON IS CLICKED.
@@ -66,72 +68,66 @@ startEl.addEventListener("click", function() {
  //THIS PART IS TO BRING BACK THE QUESTION FROM HIDING.
 
         questionBox.style.display = "block";
+        newQuestion();
     }
 )
+//EVENT LISTENER FOR EACH QUESTION.
 
-//RENDERING OF EACH QUESTION. RIGHT NOW, THE QUESTION INDEX NEEDS TO BE MANUALLY CHANGED. THIS NEEDS TO BE FIXED.
+questionBox.addEventListener("click", function(event) {
+    event.preventDefault();
+    if(event.target.matches("button")) {
+        if(event.target.textContent == myJavaQuizQuestions[questionIndex].correctAnswer) {
+            questionIndex++;
+            correctAnswerTally++;
+            newQuestion();
+        } else {
+            questionIndex++;
+            newQuestion();
+            secondsLeft = secondsLeft - 10;
+
+        }
+    }
+});
 
 var questionIndex = 0;
 
 var correctAnswerTally = 0;
 
-//for (var questionIndex.Value < myJavaQuizQuestions.length) {
+var currentQuestion = document.getElementById("questionText")
+var currentAnswerA = document.getElementById("answerAText")
+var currentAnswerB = document.getElementById("answerBText")
+var currentAnswerC = document.getElementById("answerCText")
+var currentAnswerD = document.getElementById("answerDText")
 
-    var currentQuestion = myJavaQuizQuestions[questionIndex].questionText;
-    var currentAnswerA = myJavaQuizQuestions[questionIndex].answerA;
-    var currentAnswerB = myJavaQuizQuestions[questionIndex].answerB;
-    var currentAnswerC = myJavaQuizQuestions[questionIndex].answerC;
-    var currentAnswerD = myJavaQuizQuestions[questionIndex].answerD;
+function newQuestion() {
 
-    document.getElementById("questionText").innerHTML = currentQuestion;
-    document.getElementById("answerAText").innerHTML = currentAnswerA;
-    document.getElementById("answerBText").innerHTML = currentAnswerB;
-    document.getElementById("answerCText").innerHTML = currentAnswerC;
-    document.getElementById("answerDText").innerHTML = currentAnswerD;
-
-//} //else { //THIS STOPS THE QUIZ AND TAKES THE USER TO THE SCORE AREA.
-
-   //         quizOver();
-// }
-
-//check answer function
-
-    // IF ONE OF THE CHOICES IS CLICKED, 
-
-//if(answerAClicked || answerBClicked || answerCClicked || answerDClicked) {
-    
-    // COMPARE THE CLICKED CHOICE TO THE CORRECT ANSWER.
-
-    //var correctAnswerEl = myJavaQuizQuestions[questionIndex].correctAnswer;
-
-        //IF CURRENTANSWERA IS CLICKED, THEN IS CORRECTANSWEREL = "A"? IF SO, CORRECT! IF NOT, WRONG!
-
-        //  if(answerAClicked) {
-//     if(myJavaQuizQuestions[currentQuestion][answerAClicked.id] === myJavaQuizQuestions[currentQuestion].correctAnswer) {
-//         //correct answer function
-//     } else {
-//         //wrong answer function, including taking time off timer.
-//     }
-// }
-
-        //IF CURRENTANSWERB IS CLICKED, THEN IS CORRECTANSWEREL = "B"? IF SO, CORRECT! IF NOT, WRONG!
-
-        //IF CURRENTANSWERC IS CLICKED, THEN IS CORRECTANSWEREL = "C"? IF SO, CORRECT! IF NOT, WRONG!
-
-        //IF CURRENTANSWERD IS CLICKED, THEN IS CORRECTANSWEREL = "D"? IF SO, CORRECT! IF NOT, WRONG!
-
-    
-    // IF THE CLICKED CHOICE MATCHES THE CORRECT ANSWER, RETURN "CORRECT!", ADD ONE TO THE CORRECT ANSWER TALLY, THEN ADD ONE TO THE QUESTION INDEX, AND RETURN TO THE TOP OF THE LOOP.
-
-    // IF THE CLICKED CHOICE DOES NOT MATCH THE CORRECT ANSWER, RETURN "WRONG! YOU LOST 10 SECONDS OF THE REMAINING TIME.", DEDUCT 10 SECONDS FROM THE TIMER, ADD ONE TO THE QUESTION INDEX, AND RETURN TO THE TOP OF THE LOOP.
-
-    //IF THE QUESTION INDEX GOES PAST THE ARRAY LENGTH, THE GAME IS OVER. 
+    currentQuestion.textContent = myJavaQuizQuestions[questionIndex].questionText;
+    currentAnswerA.textContent = myJavaQuizQuestions[questionIndex].answerA;
+    currentAnswerB.textContent = myJavaQuizQuestions[questionIndex].answerB;
+    currentAnswerC.textContent = myJavaQuizQuestions[questionIndex].answerC;
+    currentAnswerD.textContent = myJavaQuizQuestions[questionIndex].answerD;
+  
+} 
 
 //TIMER AREA
 
-// GAME OVER AREA
+var timerEl = document.querySelector("#timer");
 
-// QUIZ OVER FUNCTION
+var secondsLeft = 90;
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timerEl.textContent = secondsLeft;
+  
+      if(secondsLeft === 0 || myJavaQuizQuestions.length === questionIndex) {
+        clearInterval(timerInterval);
+        alert("Quiz is ended.");
+      }
+  
+    }, 1000);
+  };
+
 
 //RECORD THE REMAINING TIME IN THE SCORE AREA.  POSSIBLY MULTIPLY IT BY THE NUMBER OF CORRECT ANSWERS, OR CORRECT ANSWER TALLY.
 
